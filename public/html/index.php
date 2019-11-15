f42f3dcf7961fcc7783b5803dab30d335bfd893b
Joan Barcelo Fiol <joanbarcelofiol@gmail.com>
2019-11-15 13:00:17 +0100

    Video

--- public/html/index.php
+++ public/html/index.php
@@ -28,7 +28,7 @@ while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
     		<div class="videobackground">
               <div class="overlay"></div>
               <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-                <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
+                <source src="../img/video.mp4" type="video/mp4">
               </video>
               <div class="container h-100">
                 <div class="d-flex h-100 text-center align-items-center">

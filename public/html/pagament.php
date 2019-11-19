37fafb2ec8fd1c678eb9aa69e545c45bd58f51a8
Joan Barcelo Fiol <joanbarcelofiol@gmail.com>
2019-11-19 10:18:00 +0100

    Traduccions

--- public/html/pagament.php
+++ public/html/pagament.php
@@ -20,23 +20,23 @@ if (!(isset($_SESSION["AUTH"]))){
       <div class="bg-light rounded-top shadow-sm p-2 border border-bottom-0"><h5><i class="far fa-credit-card ml-3 mt-2"></i> <?php echo $lang[$idioma]["pay"]?></h5></div>
       <div class="bg-white rounded-bottom shadow-sm p-5 border">
         <!-- Credit card form tabs -->
-        <ul role="tablist" class="nav bg-light nav-pills nav-fill mb-3">
+        <ul role="tablist" class="nav bg-light nav-pills rounded-pill nav-fill mb-3">
           <li class="nav-item">
-            <a data-toggle="pill" href="#nav-tab-card" class="nav-link active">
+            <a data-toggle="pill" href="#nav-tab-card" class="nav-link active rounded-pill">
                                 <i class="far fa-credit-card"></i>
-                                Credit Card
+                                <?php echo $lang[$idioma]["creditCard"]?>
                             </a>
           </li>
           <li class="nav-item">
-            <a data-toggle="pill" href="#nav-tab-paypal" class="nav-link">
+            <a data-toggle="pill" href="#nav-tab-paypal" class="nav-link rounded-pill">
                                 <i class="fab fa-paypal"></i>
                                 Paypal
                             </a>
           </li>
           <li class="nav-item">
-            <a data-toggle="pill" href="#nav-tab-bank" class="nav-link">
+            <a data-toggle="pill" href="#nav-tab-bank" class="nav-link rounded-pill">
                                 <i class="fa fa-university"></i>
-                                 Bank Transfer
+                                 <?php echo $lang[$idioma]["bankTransfer"]?>
                              </a>
           </li>
         </ul>
@@ -50,13 +50,13 @@ if (!(isset($_SESSION["AUTH"]))){
           <div id="nav-tab-card" class="tab-pane fade show active">
             <form role="form">
               <div class="form-group">
-                <label for="username">Full name (on the card)</label>
-                <input type="text" name="username" placeholder="Jason Doe" required class="form-control">
+                <label for="username"><?php echo $lang[$idioma]["cardFullName"]?></label>
+                <input type="text" name="username" placeholder="<?php echo $lang[$idioma]["cardNameExample"]?>" required class="form-control">
               </div>
               <div class="form-group">
-                <label for="cardNumber">Card number</label>
+                <label for="cardNumber"><?php echo $lang[$idioma]["cardNumber"]?></label>
                 <div class="input-group">
-                  <input type="text" name="cardNumber" placeholder="Your card number" class="form-control" required>
+                  <input type="text" name="cardNumber" placeholder="<?php echo $lang[$idioma]["yourCardNumber"]?>" class="form-control" required>
                   <div class="input-group-append">
                     <span class="input-group-text text-muted">
                                                 <i class="fab fa-cc-visa mx-1"></i>
@@ -69,7 +69,7 @@ if (!(isset($_SESSION["AUTH"]))){
               <div class="row">
                 <div class="col-sm-8">
                   <div class="form-group">
-                    <label><span class="hidden-xs">Expiration</span></label>
+                    <label><span class="hidden-xs"><?php echo $lang[$idioma]["expiration"]?></span></label>
                     <div class="input-group">
                       <input type="number" placeholder="MM" name="" class="form-control" required>
                       <input type="number" placeholder="YY" name="" class="form-control" required>
@@ -78,7 +78,7 @@ if (!(isset($_SESSION["AUTH"]))){
                 </div>
                 <div class="col-sm-4">
                   <div class="form-group mb-4">
-                    <label data-toggle="tooltip" title="Three-digits code on the back of your card">CVV
+                    <label data-toggle="tooltip" title="<?php echo $lang[$idioma]["ccvTip"]?>">CVV
                                                 <i class="fa fa-question-circle"></i>
                                             </label>
                     <input type="text" required class="form-control">
@@ -88,16 +88,16 @@ if (!(isset($_SESSION["AUTH"]))){
 
 
               </div>
-              <button type="button" class="subscribe btn btn-primary btn-block rounded-pill shadow-sm"> Confirm  </button>
+              <button type="button" class="subscribe btn btn-primary btn-block rounded-pill shadow-sm"> <?php echo $lang[$idioma]["confirm"]?>  </button>
             </form>
           </div>
           <!-- End -->
 
           <!-- Paypal info -->
           <div id="nav-tab-paypal" class="tab-pane fade">
-            <p>Paypal is easiest way to pay online</p>
+            <p><?php echo $lang[$idioma]["paypalEasy"]?></p>
             <p>
-              <button type="button" class="btn btn-primary rounded-pill"><i class="fab fa-paypal mr-2"></i> Log into my Paypal</button>
+              <button type="button" class="btn btn-primary rounded-pill"><i class="fab fa-paypal mr-2"></i> <?php echo $lang[$idioma]["loginPaypal"]?></button>
             </p>
             <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
@@ -106,13 +106,13 @@ if (!(isset($_SESSION["AUTH"]))){
 
           <!-- bank transfer info -->
           <div id="nav-tab-bank" class="tab-pane fade">
-            <h6>Bank account details</h6>
+            <h6><?php echo $lang[$idioma]["bankDetails"]?></h6>
             <dl>
-              <dt>Bank</dt>
+              <dt><?php echo $lang[$idioma]["bank"]?></dt>
               <dd> THE WORLD BANK</dd>
             </dl>
             <dl>
-              <dt>Account number</dt>
+              <dt><?php echo $lang[$idioma]["bankAccount"]?></dt>
               <dd>7775877975</dd>
             </dl>
             <dl>

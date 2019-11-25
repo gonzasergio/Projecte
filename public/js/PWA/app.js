a8bba7e8c11265ab6fcfbfaa5387e67b1098fa6f
Joan Barcelo Fiol <joanbarcelofiol@gmail.com>
2019-11-25 16:19:08 +0100

    PWA Update - 3

--- public/js/PWA/app.js
+++ public/js/PWA/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('../../app/PWA/sw.js')
+      navigator.serviceWorker.register('../../js/PWA/sw.js')
         .then(function() {
             console.log('ServiceWorker registered!');
         })

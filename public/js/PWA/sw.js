f95c57f4a9b03ddad1109dde8e805775761b70f7
Joan Barcelo Fiol <joanbarcelofiol@gmail.com>
2019-11-26 10:23:59 +0100

    Fix

--- public/js/PWA/sw.js
+++ public/js/PWA/sw.js
@@ -5,27 +5,40 @@ var urlsToCache = [
   '../../img/icons/logo-512.png',
   '../../img/icons/logo.png',
   '../../img/icons/favicon.png',
-  '../css/google-fonts/fonts.css',
+  '../../css/google-fonts/fonts.css',
   '../js/crypto-js/aes.js',
-  '../js/popper/popper.min.js"',
+  '../js/popper/popper.min.js',
   '../js/bootstrap/bootstrap.min.js',
-  '../css/global.css',
-  '../css/bootstrap/bootstrap.min.css',
-  '../css/fontawesome/css/all.min.css',
+  '../../css/global.css',
+  '../../css/bootstrap/bootstrap.min.css',
+  '../../css/fontawesome/css/all.min.css',
   '../js/jquery/jquery.min.js',
   '../js/global.js'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Cache open!');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+self.addEventListener('install', e => {
+	  e.waitUntil(
+	    caches.open(CACHE_NAME).then(cache => {
+	    	return cache.addAll(urlsToCache)
+	          .then(() => self.skipWaiting());
+	    })
+	  );
+	});
+
+	self.addEventListener('activate', event => {
+	  event.waitUntil(self.clients.claim());
+	});
+
+	self.addEventListener('fetch', event => {
+	  event.respondWith(
+	    caches.open(CACHE_NAME)
+	      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+	      .then(response => {
+	      return response || fetch(event.request);
+	    })
+	  );
+	});
+	
 
 
 self.addEventListener('fetch', function(event) {
@@ -40,18 +53,3 @@ self.addEventListener('fetch', function(event) {
         })
     );
 });
-
-// self.addEventListener('activate', event => {
-//     // remove old caches
-//     event.waitUntil(
-//       caches.keys().then(keys => Promise.all(
-//         keys.map(key => {
-//           if (key != CACHE_NAME) {
-//             return caches.delete(key);
-//           }
-//         })
-//       )).then(() => {
-//         console.log('Now ready to handle fetches!');
-//       })
-//     );
-// });

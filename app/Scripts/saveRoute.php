e240a2259750fd055390b957c936ab2de3f90751
Joan Barcelo Fiol <joanbarcelofiol@gmail.com>
2019-12-17 09:59:29 +0100

    Fix LastRoute

--- app/Scripts/saveRoute.php
+++ app/Scripts/saveRoute.php
@@ -1,8 +1,8 @@
 <?php
-$lastRoute = basename($_SERVER['REQUEST_URI']);
-
-if ($lastRoute != 'registre.php' && $lastRoute != 'login.php' && substr($lastRoute, 0, 12) != 'historia.php')
+$url = explode("/", $_SERVER['REQUEST_URI']);
+$lastRoute = $url[sizeof($url)-1];
+if ($lastRoute != 'registre' && $lastRoute != 'login' && substr($lastRoute, 0, 8) != 'historia')
     $_SESSION["lastRoute"] = $lastRoute;
 
-if ($lastRoute == "html")
-    $_SESSION["lastRoute"] = "index.php";
+if ($lastRoute == "")
+    $_SESSION["lastRoute"] = "inici";

16f11b44456aa8270053768c172eb640850659ba
Joan Barcelo Fiol <joanbarcelofiol@gmail.com>
2020-02-07 08:16:59 +0100

    Opcions Perfil

--- app/Views/perfilOpcions.php
+++ app/Views/perfilOpcions.php
@@ -14,45 +14,58 @@ include '../templates/globalIclude.php';
     		<div class="col-md-3 ">
     			<h3><i class="fas fa-user-cog"></i> Opcions</h3>
     			<hr>
-    		     <div class="list-group my-3">
-                  <a href="#" class="list-group-item list-group-item-action active">Informacio personal</a>
-                  <a href="#" class="list-group-item list-group-item-action">Seguretat</a>
+    		     <div class="list-group my-3 nav nav-tab" id="Tab" role="tablist">
+                  <a id="info-tab" data-toggle="tab" href="#info" role="tab" aria-controls="info" aria-selected="true" class="list-group-item list-group-item-action active">Informacio personal</a>
+                  <a id="security-tab" data-toggle="tab" href="#security" role="tab" aria-controls="security" aria-selected="false" class="list-group-item list-group-item-action">Seguretat</a>
+                  <a id="pagament-tab" data-toggle="tab" href="#pagament" role="tab" aria-controls="pagament" aria-selected="false" class="list-group-item list-group-item-action">Pagament</a>
                 </div> 
     		</div>
     		<div class="col-md-9">
     		    <div class="card">
     		        <div class="card-body">
-    		            <div class="row">
-    		                <div class="col-md-12">
+    		            <div class="row tab-content">
+    		                <div role="tabpanel" aria-labelledby="info-tab" id="info" class="col-md-12 tab-pane active">
     		                    <form>
                                   <div class="form-group row">
-                                    <label for="username" class="col-4 col-form-label">Nom*</label> 
+                                    <label for="username" class="col-4 col-form-label">Usuari*</label> 
+                                    <div class="col-8">
+                                      <input id="username" name="username" placeholder="Usuari" class="form-control here" required="required" type="text">
+                                    </div>
+                                  </div>
+                                  <div class="form-group row">
+                                    <label for="nom" class="col-4 col-form-label">Nom*</label> 
                                     <div class="col-8">
                                       <input id="nom" name="nom" placeholder="Nom" class="form-control here" required="required" type="text">
                                     </div>
                                   </div>
                                   <div class="form-group row">
-                                    <label for="name" class="col-4 col-form-label">Primer llinatge*</label> 
+                                    <label for="llinatge1" class="col-4 col-form-label">Primer llinatge*</label> 
                                     <div class="col-8">
                                       <input id="llinatge1" name="llinatge1" placeholder="Primer llinatge" class="form-control here" type="text">
                                     </div>
                                   </div>
                                   <div class="form-group row">
-                                    <label for="lastname" class="col-4 col-form-label">Segon llinatge</label> 
+                                    <label for="llinatge2" class="col-4 col-form-label">Segon llinatge</label> 
                                     <div class="col-8">
                                       <input id="llinatge2" name="llinatge2" placeholder="Segon llinatge" class="form-control here" type="text">
                                     </div>
                                   </div>
                                   <div class="form-group row">
-                                    <label for="email" class="col-4 col-form-label">Correu*</label> 
+                                    <label for="correu" class="col-4 col-form-label">Correu*</label> 
                                     <div class="col-8">
                                       <input id="correu" name="correu" placeholder="Correu" class="form-control here" required="required" type="text">
                                     </div>
                                   </div>
                                   <div class="form-group row">
-                                    <label for="website" class="col-4 col-form-label">Telefon</label> 
+                                    <label for="telefon" class="col-4 col-form-label">Telefon</label> 
                                     <div class="col-8">
                                       <input id="telefon" name="telefon" placeholder="Telefon" class="form-control here" type="text">
+                                    </div>
+                                  </div>
+                                  <div class="form-group row">
+                                    <label for="dni" class="col-4 col-form-label">DNI</label> 
+                                    <div class="col-8">
+                                      <input id="dni" name="dni" placeholder="DNI" class="form-control here" type="text">
                                     </div>
                                   </div>
                                   <div class="form-group row">
@@ -68,6 +81,34 @@ include '../templates/globalIclude.php';
                                   </div>
                                 </form>
     		                </div>
+    		                <div role="tabpanel" aria-labelledby="security-tab" id="security" class="col-md-12 tab-pane">
+    		                    <form>
+                                  <div class="form-group row">
+                                    <label for="passactual" class="col-4 col-form-label">Contrasenya actual*</label> 
+                                    <div class="col-8">
+                                      <input id="passactual" name="passactual" placeholder="Contrasenya actual" class="form-control here" required="required" type="password">
+                                    </div>
+                                  </div>
+                                  <div class="form-group row">
+                                    <label for="passnew" class="col-4 col-form-label">Contraseya nova*</label> 
+                                    <div class="col-8">
+                                      <input id="passnew" name="passnew" placeholder="Contraseya nova" class="form-control here" type="password">
+                                    </div>
+                                  </div>
+                                  <div class="form-group row">
+                                    <label for="repeatpassnew" class="col-4 col-form-label">Repetir Contrasenya nova*</label> 
+                                    <div class="col-8">
+                                      <input id="repeatpassnew" name="repeatpassnew" placeholder="Repetir Contrasenya nova" class="form-control here" type="text">
+                                    </div>
+                                  </div>
+                                  <div class="form-group row">
+                                    <div class="offset-4 col-8">
+                                      <button name="submit" type="submit" class="btn btn-primary">Guardar canvis</button>
+                                    </div>
+                                  </div>
+                                </form>
+    		                </div>
+    		                <div role="tabpanel" aria-labelledby="pagament-tab" id="pagament" class="col-md-12 tab-pane"></div>
     		            </div>
     		            
     		        </div>
@@ -81,6 +122,7 @@ include '../templates/globalIclude.php';
         $(".clickable-row").click(function() {
             window.location = $(this).data("href");
         });
+        $('#info').tab('show')
     </script>
 </body>
 </html>
